a621af3994472b231f0049afe2942ff8aa56e888
LawftyGoals <mymsnadressis@hotmail.com>
2022-08-07 17:59:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Friend Tracker</h1>
+      <div>
+        <img
+          src={process.env.PUBLIC_URL + '/my-profile-pic.png'}
+          alt = "Shaun Smiling"
+          height="200" />
+          <h2>My Profile</h2>
+          <h3>Name</h3>
+          <p>Oli</p>
+          <h3>Age</h3>
+          <p>100</p>
+          <h3>Bio</h3>
+          <p>I like to program and eat food.</p>
+          <h3>Birthday</h3>
+          <p>August 1st</p>
+          <h3>Interests</h3>
+          <ul>
+            <li>Programming</li>
+            <li>Data Science</li>
+          </ul>
+      </div>
     </div>
+
   );
 }
 
